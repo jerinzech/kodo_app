7185fb7a690c982ea8f490184ce37885bff0d105
jerinzech <jerinzjames@gmail.com>
2022-10-24 14:36:14 +0530

    added folder font and folder color functionality

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 // colors for the app
-class AppColors {}
+class AppColors {
+  static const Color liteGreen = Color(0xff7FCB84);
+  static const Color textBlack = Colors.black;
+  static const Color cream = Color(0xffF7E0CE);
+  static const Color liteOrange = Color(0xffF89178);
+  static const Color orange = Color(0xffFE9D58);
+  static const Color liteBlue = Color(0xff74B8BA);
+  static const Color purple = Color(0xffA586D0);
+  static const Color sageBlue = Color(0xff7B88BB);
+  static const Color sageGrey = Color(0xffB3AFB7);
+}
 
 TextStyle robotoMonoStyle = GoogleFonts.robotoMono();
 TextStyle montserratStyle = GoogleFonts.montserrat();
--- lib/widgets/folderadd.dart
+++ lib/widgets/folderadd.dart
@@ -14,6 +14,35 @@ class FolderAdd extends StatelessWidget {
   //   required this.onCancel,
   // });
 
+  List folderColorList = [
+    AppColors.liteGreen,
+    AppColors.textBlack,
+    AppColors.cream,
+    AppColors.liteOrange,
+    AppColors.orange,
+    AppColors.liteBlue,
+    AppColors.purple,
+    AppColors.sageBlue,
+    AppColors.sageGrey
+  ];
+
+  List folderFontList = [
+    righteousStyle.copyWith(
+        fontSize: 20, fontWeight: FontWeight.normal, color: Colors.white),
+    robotoMonoStyle.copyWith(
+        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+    concertOneStyle.copyWith(
+        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+    kanitStyle.copyWith(
+        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+    yesevaOneStyle.copyWith(
+        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+    dmSansStyle.copyWith(
+        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+    playfairDisplayStyle.copyWith(
+        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white)
+  ];
+
   @override
   Widget build(BuildContext context) {
     final mediaPadding = MediaQuery.of(context);
@@ -36,9 +65,11 @@ class FolderAdd extends StatelessWidget {
             height: 400,
             child: Column(
               mainAxisSize: MainAxisSize.min,
-              // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Spacer(),
+                SizedBox(
+                  height: 10,
+                ),
                 Padding(
                   padding: const EdgeInsets.all(5.0),
                   child: Container(
@@ -78,8 +109,108 @@ class FolderAdd extends StatelessWidget {
                         )),
                   ),
                 ),
-                Spacer(),
-                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: SizedBox(
+                    height: 80,
+                    child: Container(
+                        color: Colors.grey[900],
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.only(left: 10, top: 2),
+                              height: 20,
+                              child: Text(
+                                "folder color",
+                                textAlign: TextAlign.left,
+                                style: montserratStyle.copyWith(
+                                  fontSize: 15,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              height: 60,
+                              child: GridView.builder(
+                                  physics: NeverScrollableScrollPhysics(),
+                                  itemCount: 8,
+                                  gridDelegate:
+                                      SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 8,
+                                  ),
+                                  itemBuilder: (context, index) {
+                                    return Container(
+                                      height: 25,
+                                      width: 25,
+                                      padding: const EdgeInsets.all(5.0),
+                                      child: TextButton(
+                                        autofocus: true,
+                                        onPressed: () {},
+                                        child: Container(
+                                          height: 35,
+                                          width: 35,
+                                          color: folderColorList[index],
+                                        ),
+                                      ),
+                                    );
+                                  }),
+                            ),
+                          ],
+                        )),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: SizedBox(
+                    height: 120,
+                    child: Container(
+                        color: Colors.grey[900],
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.only(left: 10, top: 2),
+                              height: 20,
+                              child: Text(
+                                "folder font",
+                                textAlign: TextAlign.left,
+                                style: montserratStyle.copyWith(
+                                  fontSize: 15,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              height: 100,
+                              child: GridView.builder(
+                                  // physics: NeverScrollableScrollPhysics(),
+                                  itemCount: 6,
+                                  gridDelegate:
+                                      SliverGridDelegateWithFixedCrossAxisCount(
+                                    mainAxisExtent: 50.0,
+                                    crossAxisCount: 3,
+                                  ),
+                                  itemBuilder: (context, index) {
+                                    return Container(
+                                      padding: const EdgeInsets.all(5.0),
+                                      child: TextButton(
+                                        autofocus: true,
+                                        onPressed: () {},
+                                        child: Text(
+                                          'Hello',
+                                          style: folderFontList[index],
+                                        ),
+                                      ),
+                                    );
+                                  }),
+                            ),
+                          ],
+                        )),
+                  ),
+                ),
                 Row(
                   children: <Widget>[
                     Expanded(
